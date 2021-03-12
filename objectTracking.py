3e4a990fec6eda35e55ad2384b690c873903a8e4
Hedayet Islam <islamhedayet67@gmail.com>
2021-03-13 01:21:22 +0600

    Updating Dog and cat

--- objectTracking.py
+++ objectTracking.py
@@ -1,1 +1,1 @@
-import cv2 as cv
+import tensorflow as tf
--- predictionOfcatsAndDogs.py
+++ predictionOfcatsAndDogs.py
@@ -8,10 +8,12 @@ import cv2 as cv
 new_model = load_model('catAndDog50epochs.h5')
 
 
-
 # predict on new images
 
-dog_file ='CATS_DOGS/CATS_DOGS/test/DOG/9650.jpg'
+dog_file ='CATS_DOGS/CATS_DOGS/test/DOG/9656.jpg'
+
+img = cv.imread(dog_file)
+
 from tensorflow.keras.preprocessing import image
 dog_img = image.load_img(dog_file,target_size=(150,150))
 dog_img = image.img_to_array(dog_img)
@@ -21,4 +23,12 @@ dog_img = np.expand_dims(dog_img,axis = 0)
 
 dog_img = dog_img/255
 print(new_model.predict(dog_img))
-print(new_model.predict_classes(dog_img))
+res =new_model.predict_classes(dog_img)
+print(res[0][0])
+if res[0][0]==1:
+    print("Its a dog ")
+else:
+    print("Its a pussy")
+cv.imshow('image',img)
+
+cv.waitKey(0)
